2f43be8b51a2822874545678f1750ba32d9aa24c
Lucky Himawan Prasetya <162990675+Ckyyy002@users.noreply.github.com>
2025-05-11 19:09:43 +0700

    Create 10.c

---
+++ Task_5/10.c
@@ -0,0 +1,18 @@
+10_147.c
+#include <stdio.h>
+
+void order(int *bst, int i, int n) {
+    if (i > n) {
+        return;
+    }
+
+    order(bst, 2 * i, n);
+    order(bst, 2 * i + 1, n);
+    printf("%d ", *(bst + i));
+}
+
+int main() {
+    int bst[14] = {0, 39, 21, 64, 18, 30, 56, 73, 13, 26, 47, 61, 75, 35};
+    order(bst, 1, 13);
+    return 0;
+}
